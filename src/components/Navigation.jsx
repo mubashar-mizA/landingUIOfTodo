8c989ef14d25c95765f8b16c0d27904945fdf58f
mubashar-mizA <mirzamubashar535@gmail.com>
2024-11-22 12:52:35 +0500

    add a dummy Naviagation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 const Navigation = () => {
     return (
-        <div className='flex justify-center items-center gap-x-20 my-5'>
+        <div className='md:flex justify-center items-center md:gap-x-20 md:my-5 p-2 md:p-0'>
 
             <h1 className=' text-green-600'>
                 Welcome back <strong>mirzamubashar533@gmail.com</strong>
             </h1>
-            <ul className='flex gap-5'>
+            <ul className='flex gap-6'>
                 <li>Logout</li>
                 <li>Trash</li>
             </ul>
